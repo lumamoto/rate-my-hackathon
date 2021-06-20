69b79792feee4da8dce4920a234d3856d66357ec
Lacey Umamoto <lcumamoto@gmail.com>
2021-06-20 16:30:38 -0700

    update procfile

--- frontend/src/components/utils/axiosConfig.js
+++ frontend/src/components/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const env = process.env.NODE_ENV; // current environment
-const atlas_uri = process.env.ATLAS_URL; // current environment
+const atlas_uri = process.env.ATLAS_URI; // current environment
 
 export const app = axios.create({
   baseURL:
