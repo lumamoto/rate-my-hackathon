a8732d20d0a888f6603cf62ca8d73d125570fb25
Lacey Umamoto <lcumamoto@gmail.com>
2021-06-23 18:54:47 -0700

    serving static files

--- backend/app.js
+++ backend/app.js
@@ -16,10 +16,20 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+const path = require('path')
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
 app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/hackathons', hackathons);
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+})
 
 const port = process.env.PORT || 8082;
 
