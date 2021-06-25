725f7ad85418e24ee9b6e52f9c3d581f3f920acb
Lacey Umamoto <lcumamoto@gmail.com>
2021-06-24 20:31:36 -0700

    update base url

--- frontend/src/components/utils/axiosConfig.js
+++ frontend/src/components/utils/axiosConfig.js
@@ -4,11 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config({path: '/.env'});
 
 const atlas_uri = process.env.ATLAS_URI;
-const env = process.env.NODE_ENV; // current environment
+// const env = process.env.NODE_ENV; // current environment
 
 export const app = axios.create({
-  baseURL:
-    env === 'prod'
-      ? atlas_uri + '/api/hackathons' // production
-      : 'http://localhost:8082/api/hackathons', // development
+  baseURL: atlas_uri + '/api/hackathons'
+    // env === 'prod'
+    //   ? atlas_uri + '/api/hackathons' // production
+    //   : 'http://localhost:8082/api/hackathons', // development
 });
