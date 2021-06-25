068893186029ecf2459b4ad8b169014a05414c7c
Lacey Umamoto <lcumamoto@gmail.com>
2021-06-24 20:09:11 -0700

    update axiosConfig

--- frontend/src/components/utils/axiosConfig.js
+++ frontend/src/components/utils/axiosConfig.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const env = process.env.NODE_ENV; // current environment
+// const env = process.env.NODE_ENV; // current environment
 const atlas_uri = process.env.ATLAS_URI; // current environment
 
 export const app = axios.create({
-  baseURL:
-    env === 'prod'
-      ? atlas_uri + '/api/hackathons' // production
-      : 'http://localhost:8082/api/hackathons', // development
+  baseURL: atlas_uri + '/api/hackathons'
+    // env === 'prod'
+    //   ? atlas_uri + '/api/hackathons' // production
+    //   : 'http://localhost:8082/api/hackathons', // development
 });
